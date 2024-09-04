4bde2684205e3c624f5a300fd2ec5c30c6856ce4
heppaat <pali.herrer@gmail.com>
2024-09-04 18:08:48 +0200

    backend setup with get endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello world")
+import express from "express";
+import fs from "fs/promises";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const readFile = async () => {
+  try {
+    const rawData = await fs.readFile(
+      `${__dirname}/../backend/data.json`,
+      "utf-8"
+    );
+    const data = JSON.parse(rawData);
+    return data;
+  } catch (error) {
+    console.error("Error reading file", error);
+    return null;
+  }
+};
+
+app.get("/posts", async (req, res) => {
+  const posts = await readFile();
+  if (!posts) return res.sendStatus(500);
+
+  res.json(posts);
+});
+
+app.listen(4002);
