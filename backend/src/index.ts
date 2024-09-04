7053833798ef60b932abebad6ba4c3d1512d1b17
heppaat <pali.herrer@gmail.com>
2024-09-04 18:36:48 +0200

    backend post endpoint created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs/promises";
 import cors from "cors";
+import { error } from "console";
 
 const app = express();
 
@@ -26,6 +27,36 @@ app.get("/posts", async (req, res) => {
   if (!posts) return res.sendStatus(500);
 
   res.json(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const result = req.body;
+  if (!result || !result.userId || !result.title || !result.body) {
+    return res.status(400).json({ error: "Invalid post data" });
+  }
+
+  const posts = await readFile();
+  if (!posts) return res.sendStatus(500);
+
+  const newId = Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+
+  const newPost = {
+    userId: result.userId,
+    id: newId,
+    title: result.title,
+    body: result.body,
+  };
+
+  try {
+    await fs.writeFile(
+      `${__dirname}/../database/data.json`,
+      JSON.stringify([...posts, newPost], null, 2)
+    );
+    res.json(newPost);
+  } catch (error) {
+    console.error("Failed to save new post", error);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(4002);
